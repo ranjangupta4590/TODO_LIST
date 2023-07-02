d8cc4249ba0c8d6a0f7ec5d88b9a768f55f7ccd0
ranjan_4590 <ranjangupta35558@gmail.com>
2023-07-03 00:48:50 +0530

    button

--- frontend/src/component/Input.js
+++ frontend/src/component/Input.js
@@ -24,7 +24,7 @@ const Input = () => {
                     <input className=' lg:w-[30rem] sm:w-[20rem] xsm:w-[10rem] text-2xl  border-b-[2px] border-pink-400 outline-none' type='text' placeholder='Enter your work...' value={data} onChange={(e) => setData(e.target.value)} required />
                 </div>
                 <div className='pt-2'>
-                    <button type='submit' className=' text-[20px] rounded-full pl-4 pr-4 items-center bg-pink-600 text-white hover:bg-blue-600 hover:scale-110 transition-all duration-300 ' onClick={update ? () => updateTodo(todoId, data, setData, setTodo, setUpdate) : () => addTodo(data, setData, setTodo)}>
+                    <button type='submit' className=' text-[20px] rounded pl-4 pr-4 items-center bg-pink-600 text-white hover:bg-blue-600 hover:scale-110 transition-all duration-300 ' onClick={update ? () => updateTodo(todoId, data, setData, setTodo, setUpdate) : () => addTodo(data, setData, setTodo)}>
                         {update ? "Update" : "Add"}
                     </button>
                 </div>
